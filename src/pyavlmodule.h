e08109a813a6491de2031851aec111ae1251e582
wormtooth <ye@wormtooth.com>
2020-11-15 16:20:54 -0500

    add extern to make clang work

--- src/pyavlmodule.h
+++ src/pyavlmodule.h
@@ -5,10 +5,10 @@
 #define PYAVL_VERSION_MINOR 1
 #define PYAVL_VERSION_MICRO 0
 
-PyTypeObject TreeSet_Type;
+extern PyTypeObject TreeSet_Type;
 #define TreeSetObj_Check(obj)    (Py_TYPE(obj) == &TreeSet_Type)
 
-PyTypeObject TreeMap_Type;
+extern PyTypeObject TreeMap_Type;
 #define TreeMapObj_Check(obj)    (Py_TYPE(obj) == &TreeMap_Type)
 
 /* TreeIter_Type */
@@ -19,7 +19,7 @@ PyTypeObject TreeMap_Type;
  */
 typedef PyObject* (*avl_iter_getter)(avl_node_t *);
 
-PyTypeObject TreeIter_Type;
+extern PyTypeObject TreeIter_Type;
 #define TreeIterObj_Check(obj)    (Py_TYPE(obj) == &TreeIter_Type)
 
 extern PyObject*
